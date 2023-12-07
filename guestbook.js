1594a3f3c1af60c055202208fb0fc0a1f77d3bfe
Robin Bühler <git+r@obin.ch>
2023-12-07 16:15:20 +0100

    chore: externalize guestbook script

---
+++ guestbook.js
@@ -0,0 +1,35 @@
+module.exports = async ({github, context}) => {
+  const query = `query($owner:String!, $name:String!, $issue_number:Int!) {
+    repository(owner:$owner, name:$name){
+      issue(number:$issue_number) {
+        comments(first:10,orderBy:{direction:DESC, field:UPDATED_AT}) {
+          nodes {
+            author {
+              avatarUrl(size: 24)
+              login
+              url
+            }
+            bodyText
+            updatedAt
+          }
+        }
+      }
+    }
+  }`;
+  const variables = {
+    owner: context.repo.owner,
+    name: context.repo.repo,
+    issue_number: context.issue.number
+  }
+  const result = await github.graphql(query, variables)
+  const renderComments = (comments) => {
+    return comments.reduce((prev, curr) => {
+      const sanitizedText = curr.bodyText.replace(/(\r\n|\r|\n)/g, "<br />").replace('|', '&#124;').replace('<', '&lt;').replace('>', '&gt;').replace('[', '&#91;');
+      return `${prev}|[![${curr.author.login}](${curr.author.avatarUrl})<br />${curr.author.login}](${curr.author.url})|${new Date(curr.updatedAt).toLocaleString()}|${sanitizedText}|\n`;
+    }, "| Name | Date | Message |\n|---|---|---|\n");
+  };
+
+  const fileSystem = require('fs');
+  const readme = fileSystem.readFileSync('README.md', 'utf8');
+  fileSystem.writeFileSync('README.md', readme.replace(/(?<=<!-- Guestbook -->.*\n)[\S\s]*?(?=<!-- \/Guestbook -->|$(?![\n]))/gm, renderComments(result.repository.issue.comments.nodes)), 'utf8');
+}
